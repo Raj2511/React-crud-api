655138585342f64c67646a35fbb8fdf8f69a8dcb
Raj2511 <rahul25111999@gmail.com>
2024-07-17 21:48:36 +0530

    commit put, delete api's

--- functions/index.js
+++ functions/index.js
@@ -25,67 +25,126 @@ const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 //permissions
 var serviceAccount = require("./permissions.json");
 const { QuerySnapshot } = require('firebase-admin/firestore');
 admin.initializeApp({
-credential: admin.credential.cert(serviceAccount),
-databaseURL: "https://react-crud-2b469..firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://react-crud-2b469..firebaseio.com"
 });
 const db = admin.firestore();
 
 
 //test function
 app.get('/hello-world', (req, res) => {
-    return res.status(200).send('hello world!');
+  return res.status(200).send('hello world!');
 
 });
 
 //Create - API
 app.post('/api/create', (req, res) => {
-    (async () => {
-      try {
-        await db.collection('Users').doc().set({
-          City: req.body.City,
-          MailId: req.body["Mail Id"],
-          Name: req.body.Name,
-          isActive: req.body.isActive
-        });
-        return res.status(200).send();
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
-      }
-    })();
-  });
+  (async () => {
+    try {
+      await db.collection('Users').doc().set({
+        City: req.body.City,
+        MailId: req.body["Mail Id"],
+        Name: req.body.Name,
+        isActive: req.body.isActive
+      });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
-  //Read - API
-  app.get('/api/read', (req, res) =>{
-    (async () => {
-      try{
-        let query = db.collection('Users');
-        let response = [];
-        await query.get().then(QuerySnapshot =>{
-          let docs = QuerySnapshot.docs;
-          for(let doc of docs){
-            const selectedItem = {
-              id: doc.id,
-              name: doc.data().Name,
-              city: doc.data().City,
-              mailId: doc.data().MailId
-            };
-            response.push(selectedItem);
-          }
-        });
-        return res.status(200).send(response);
-      }
-      catch(error){
-        console.log(error);
-        return res.status(500).send(error);
-      }
-    })();
-  });
+//API: http://127.0.0.1:5001/react-crud-2b469/us-central1/app/api/create
+//Sample body
+//   {
+//     "City": "chennai",
+//     "Mail Id": "raj2@gmail.com",
+//     "Name": "Rahul",
+//     "isActive": true
+// }
+
+//Read - API
+app.get('/api/read', (req, res) => {
+  (async () => {
+    try {
+      let query = db.collection('Users');
+      let response = [];
+      await query.get().then(QuerySnapshot => {
+        let docs = QuerySnapshot.docs;
+        for (let doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            name: doc.data().Name,
+            city: doc.data().City,
+            mailId: doc.data().MailId
+          };
+          response.push(selectedItem);
+        }
+      });
+      return res.status(200).send(response);
+    }
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+//API: http://127.0.0.1:5001/react-crud-2b469/us-central1/app/api/read
+
+
+//Update - API
+app.put('/api/update/:item_id', (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection('Users').doc(req.params.item_id);
+      await document.update({
+        City: req.body.City,
+        MailId: req.body["Mail Id"],
+        Name: req.body.Name,
+        isActive: req.body.isActive
+      });
+      return res.status(200).send();
+    }
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+//API: http://127.0.0.1:5001/react-crud-2b469/us-central1/app/api/update/:item_id
+//should pass id in parameter
+//Sample body
+// {
+//   "City": "new chennai",
+//    "Mail Id": "newraj2@gmail.com",
+//    "Name": "new Rahul",
+//    "isActive": false
+// }
+
+//Delete - API
+app.delete('/api/delete/:item_id', (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection('Users').doc(req.params.item_id);
+      await document.delete();
+      return res.status(200).send();
+    }
+    catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+//API: http://127.0.0.1:5001/react-crud-2b469/us-central1/app/api/delete/:item_id
+//should pass id
 
 exports.app = functions.https.onRequest(app);
